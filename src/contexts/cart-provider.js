2759241f9e981c09cc477ec014e0dac3aff68b9d
tomzacchia <tomzacchia@live.com>
2022-02-01 19:01:31 -0800

    NOTE: cart-provider

--- src/contexts/cart-provider.js
+++ src/contexts/cart-provider.js
@@ -1,16 +1,57 @@
-const { default: CartContext } = require("./cart-context");
+import { useReducer } from "react";
+
+import CartContext from "./cart-context.context";
+
+/*
+  NOTE: we can manage context value inside of <App /> however if there were other
+  contexts at App root level we would quickly bloat the function body of <App />.
+  As such we will manage the state of the Cart context within a higher-order
+  component
+*/
+
+const DEFAULT_STATE = {
+  items: [],
+  totalAmount: 0,
+};
+
+const ADD = "ADD";
+const REMOVE = "REMOVE";
+
+function cartReducer(state, action) {
+  switch (action.type) {
+    case ADD:
+      const { item } = action.payload;
+      const updatedItems = state.items.concat(item);
+      const totalAmount = updatedItems.reduce(
+        (acc, item) => acc + item.amount * item.price,
+        0
+      );
+      return { items: updatedItems, totalAmount: totalAmount };
+
+    case REMOVE:
+      return state;
+    default:
+      return DEFAULT_STATE;
+  }
+}
 
 function CartProvider(props) {
+  const [cartState, dispatchCart] = useReducer(cartReducer, DEFAULT_STATE);
+
+  function addItemToCartHandler(item) {
+    dispatchCart({ type: ADD, payload: item });
+  }
+
+  function removeItemFromCartHandler(id) {
+    dispatchCart({ type: REMOVE, payload: id });
+  }
+
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
-    removeItem: removeItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
   };
-
-  function addItemToCartHandler(item) {}
-
-  function removeItemToCartHandler(id) {}
 
   return (
     <CartContext.Provider value={cartContext}>
