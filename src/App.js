770b13a052a6385a2543eed060deb29cb7d50ff7
tomzacchia <tomzacchia@live.com>
2022-02-01 16:50:45 -0800

    Add showCart state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "features/Header";
 import Meals from "features/Meals";
 import Cart from "features/Cart";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  function toggleShowCart() {
+    setShowCart((prevState) => !prevState);
+  }
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {showCart && <Cart onHideCart={toggleShowCart} />}
+
+      <Header onShowCart={toggleShowCart} />
       <main>
         <Meals />
       </main>
