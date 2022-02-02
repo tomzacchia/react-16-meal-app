a9034f47a4f7f5529ebe5cecaafa7037a19712cb
tomzacchia <tomzacchia@live.com>
2022-02-01 19:40:12 -0800

    Add forwardRef to Input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,13 +1,14 @@
+import React from "react";
 import classes from "./input.module.css";
 
-function Input(props) {
+function Input(props, ref) {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/* NOTE: We spread object key-value pairs to add it as attributes on input */}
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
 }
 
-export default Input;
+export default React.forwardRef(Input);
